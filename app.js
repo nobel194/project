50887133aca88354bb5f3d7b51350c503eae5abe
Nobel <hollow.sky2@gmail.com>
2019-04-27 07:28:12 +0545

    chages committed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
-const conn = require('./database.js');
 
-app.get('/',function(request,response){
+const connect = require('./database.js');
+
+const app = express();
+
+app.get('/',function(request, response){
 	response.send('Hello World');
 })
 
@@ -20,11 +22,4 @@ app.listen(3000,function(){
 	console.log('Server running at localhost:'+3000);
 })
 
-function dba(){
-	mongoose.db("mongodb://localhost:27017/myapp");
-	return mongoose.connection;
-}
-
-dba()
-.on('error', function(){console.log('Database connection error',err)})
-.on('connected',function(){AudioListener()});
+connect()
--- database.js
+++ database.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
-const port = 3000;
-const connection = mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
-console.log(connection);
-const user=require('./schema').default;
+mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
 const db = mongoose.connection;
-db.on('error', function(err) {console.log('Database connection error',err)});
-db.once('connected', function() {
-  console.log("we're connected!");
-});
-function dba(){
-	mongoose.db("mongodb://localhost:27017/myapp");
-	return mongoose.connection;
+function connect() {
+	db.on('error', function(err) {console.log('Database connection error',err)});
+	db.once('connected', function() {
+	  console.log("connected to mongodb!");
+	});
 }
 
-dba()
-.on('error', function(){console.log('Database connection error',err)})
-.on('connected',function(){AudioListener()});
+module.exports = connect;
