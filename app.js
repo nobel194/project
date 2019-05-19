b138d24bf0131557e86b7d3bbfcdb07d13b02c38
Nobel <hollow.sky2@gmail.com>
2019-05-19 12:07:35 +0545

    New file route where routes are seperately written

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-const Blog = require('./model/blog.js');
 const db = require('./database.js');
+const Routes = require('./route.js');
 
 const app = express();
 
 app.use(bodyParser.json());
-
-app.get('/', function(request, response){
-	response.send('Hello World');
-})
-
-app.get('/blogs', function(request, response){
-	Blog.find({}, function (err, docs) {
-		if(err) {
-			response.send({error: err.message });
-		}
-		response.send(docs);
-	});
-})
-
-app.post('/blogs', function(request, response){
-	const data = request.body;
-	console.log(data);
-	Blog.insertMany([data], function(err, docs) {
-		if(err) {
-			response.send({error: err.message });
-		}
-	response.send(docs);
-	});
-})
+app.use(Routes);
 
 db.on('error', function(err) {console.log('Database connection error',err)});
 db.once('connected', function() {
---
+++ route.js
@@ -0,0 +1,35 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const Blog = require('./model/blog.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', function(request, response){
+	response.send('Hello World');
+})
+
+app.get('/blogs', function(request, response){
+	Blog.find({}, function (err, docs) {
+		if(err) {
+			response.send({error: err.message });
+		}
+		response.send(docs);
+	});
+})
+
+app.post('/blogs', function(request, response){
+	const data = request.body;
+	console.log(data);
+	Blog.insertMany([data], function(err, docs) {
+		if(err) {
+			response.send({error: err.message });
+		}
+	response.send(docs);
+	});
+})
+
+
+module.exports = app;
