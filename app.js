e7ff11e51febe527372074b43105ed616b86a425
Nobel <hollow.sky2@gmail.com>
2019-05-19 10:52:31 +0545

    Changes made

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = require('./database.js');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/', function(request, response){
 	response.send('Hello World');
@@ -23,6 +23,7 @@ app.get('/blogs', function(request, response){
 
 app.post('/blogs', function(request, response){
 	const data = request.body;
+	console.log(data);
 	Blog.insertMany([data], function(err, docs) {
 		if(err) {
 			response.send({error: err.message });
@@ -35,7 +36,7 @@ db.on('error', function(err) {console.log('Database connection error',err)});
 db.once('connected', function() {
   console.log("connected to mongodb!");
 
-  app.listen(3000,function(){
-	console.log('Server running at localhost:'+3000);
+  app.listen(5000,function(){
+	console.log('Server running at localhost:'+5000);
   })
 });
